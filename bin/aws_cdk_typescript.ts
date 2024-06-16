0cd79e1421329f3e59a9cbe5a905797d3be40185
Syota0622 <wvp360gqanukcqhg@gmail.com>
2024-06-16 14:48:57 +0900

    import 名を修正

--- bin/aws_cdk_typescript.ts
+++ bin/aws_cdk_typescript.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AwsCdkTypescriptStack } from '../lib/aws_cdk_typescript-stack';
+import { MyVpcProjectStack } from '../lib/my-vpc-project-stack';
 
 const app = new cdk.App();
-new AwsCdkTypescriptStack(app, 'AwsCdkTypescriptStack', {
+new MyVpcProjectStack(app, 'MyVpcProjectStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
