97a870d49e751f1b6a89e4f7f5b719a18fbab2de
Syota0622 <wvp360gqanukcqhg@gmail.com>
2024-06-16 16:28:49 +0900

    ファイル名変更

--- bin/aws_cdk_typescript.ts
+++ bin/aws_cdk_typescript.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { MyVpcProjectStack } from '../lib/my-vpc-project-stack';
-import { MyS3BucketStack } from '../lib/my-s3-bucket-stack';
+import { MyVpcProjectStack } from '../lib/vpc';
+import { MyS3BucketStack } from '../lib/s3';
 
 const app = new cdk.App();
 new MyVpcProjectStack(app, 'MyVpcProjectStack');
