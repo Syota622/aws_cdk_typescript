d496982130952332206e1284e3591d412899c2a7
Syota0622 <wvp360gqanukcqhg@gmail.com>
2024-06-16 14:18:30 +0900

    L1に修正

--- lib/my-vpc-project-stack.ts
+++ lib/my-vpc-project-stack.ts
@@ -7,44 +7,46 @@ export class MyVpcProjectStack extends cdk.Stack {
     super(scope, id, props);
 
     // VPCを作成
-    const vpc = new ec2.Vpc(this, 'MyVpc', {
-      cidr: "10.0.0.0/16",
-      maxAzs: 3,
-      natGateways: 1,
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: 'public-subnet',
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-        {
-          cidrMask: 24,
-          name: 'private-subnet',
-          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-        }
-      ],
+    const vpc = new ec2.CfnVPC(this, 'MyVpc', {
+      cidrBlock: "10.0.0.0/16",
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      tags: [{ key: 'Name', value: 'MyVpc' }]
     });
 
     // インターネットゲートウェイを作成
     const igw = new ec2.CfnInternetGateway(this, 'InternetGateway', {
-      tags: [{ key: 'Name', value: 'MyInternetGateway' }],
+      tags: [{ key: 'Name', value: 'MyInternetGateway' }]
     });
 
     // VPCにインターネットゲートウェイをアタッチ
     new ec2.CfnVPCGatewayAttachment(this, 'VpcGatewayAttachment', {
-      vpcId: vpc.vpcId,
-      internetGatewayId: igw.ref,
+      vpcId: vpc.ref,
+      internetGatewayId: igw.ref
+    });
+
+    // パブリックサブネットを作成
+    const publicSubnet = new ec2.CfnSubnet(this, 'PublicSubnet', {
+      vpcId: vpc.ref,
+      cidrBlock: "10.0.1.0/24",
+      availabilityZone: cdk.Stack.of(this).availabilityZones[0],
+      mapPublicIpOnLaunch: true,
+      tags: [{ key: 'Name', value: 'PublicSubnet' }]
+    });
+
+    // プライベートサブネットを作成
+    const privateSubnet = new ec2.CfnSubnet(this, 'PrivateSubnet', {
+      vpcId: vpc.ref,
+      cidrBlock: "10.0.2.0/24",
+      availabilityZone: cdk.Stack.of(this).availabilityZones[0],
+      tags: [{ key: 'Name', value: 'PrivateSubnet' }]
     });
 
     // 出力: VPC ID
-    new cdk.CfnOutput(this, 'VpcId', { value: vpc.vpcId });
+    new cdk.CfnOutput(this, 'VpcId', { value: vpc.ref });
     // 出力: パブリックサブネットID
-    vpc.publicSubnets.forEach((subnet, index) => {
-      new cdk.CfnOutput(this, `PublicSubnet${index + 1}Id`, { value: subnet.subnetId });
-    });
+    new cdk.CfnOutput(this, 'PublicSubnetId', { value: publicSubnet.ref });
     // 出力: プライベートサブネットID
-    vpc.privateSubnets.forEach((subnet, index) => {
-      new cdk.CfnOutput(this, `PrivateSubnet${index + 1}Id`, { value: subnet.subnetId });
-    });
+    new cdk.CfnOutput(this, 'PrivateSubnetId', { value: privateSubnet.ref });
   }
 }
